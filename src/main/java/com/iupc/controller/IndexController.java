d48eedd19ccf9b702fa6f6702a9df3b73a60e246
sunhuadi <shd_student@163.com>
2020-06-23 21:24:11 +0800

    配置service+测试axios

--- src/main/java/com/iupc/controller/IndexController.java
+++ src/main/java/com/iupc/controller/IndexController.java
@@ -2,32 +2,46 @@ package com.iupc.controller;
 
 import com.iupc.Mapper.ZixunMapper;
 import com.iupc.pojo.zixun;
+import com.iupc.service.IIndexService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.text.DateFormat;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 public class IndexController {
     @Autowired
-    ZixunMapper zx;
+    private IIndexService iis;
     @GetMapping("/")
     public String index(Model model)
     {
-        String formatDate = null;
-        System.out.println("ok?");
-        List<zixun> zxlist=zx.qurryzixunall();
-        for (int i=0;i<zxlist.size();i++)
-        {
-            zxlist.get(i).setFormattime(DateFormat.getDateInstance(DateFormat.FULL).format(zxlist.get(i).getTime()));
-            System.out.println(zxlist.get(i).getFormattime());
-        }
-
+        List<zixun> zxlist=iis.getAll();
         model.addAttribute("zxlist",zxlist);
-
         return "index";
     }
+    @ResponseBody
+    @GetMapping("/test")
+    public List<HashMap<String,String> >Test()
+    {
+        List<HashMap<String,String> >list = new ArrayList<HashMap<String, String>>();
+
+        for (int i=0;i<2;i++)
+        {
+            HashMap<String,String> mp=new HashMap<String,String>();
+           mp.put("name","sunhuadi"+i);
+                list.add(i,mp);
+        }
+
+        return list;
+    }
+
 }
---
+++ src/main/java/com/iupc/service/IIndexService.java
@@ -0,0 +1,9 @@
+package com.iupc.service;
+
+import com.iupc.pojo.zixun;
+
+import java.util.List;
+
+public interface IIndexService {
+    List<zixun> getAll();
+}
