be7927fa903c05400acb358a18050cfea3b250b4
sunhuadi <shd_student@163.com>
2020-06-30 08:58:07 +0800

    新增三个类

---
+++ src/main/java/com/iupc/pojo/Shop.java
@@ -0,0 +1,51 @@
+package com.iupc.pojo;
+
+
+public class Shop {
+
+  private String shop_id;
+  private String shop_name;
+  private String shop_password;
+  private String shop_introduce;
+  private String shop_pic;
+
+  public String getShop_id() {
+    return shop_id;
+  }
+
+  public void setShop_id(String shop_id) {
+    this.shop_id = shop_id;
+  }
+
+  public String getShop_name() {
+    return shop_name;
+  }
+
+  public void setShop_name(String shop_name) {
+    this.shop_name = shop_name;
+  }
+
+  public String getShop_password() {
+    return shop_password;
+  }
+
+  public void setShop_password(String shop_password) {
+    this.shop_password = shop_password;
+  }
+
+  public String getShop_introduce() {
+    return shop_introduce;
+  }
+
+  public void setShop_introduce(String shop_introduce) {
+    this.shop_introduce = shop_introduce;
+  }
+
+  public String getShop_pic() {
+    return shop_pic;
+  }
+
+  public void setShop_pic(String shop_pic) {
+    this.shop_pic = shop_pic;
+  }
+}
