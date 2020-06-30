ae7959c29cf893849aef8ad7b8f4070ea388208e
sunhuadi <shd_student@163.com>
2020-06-30 08:56:49 +0800

    后端News类以及Mapper配置，前端结合使用

---
+++ src/main/java/com/iupc/Mapper/UsersMapper.java
@@ -0,0 +1,11 @@
+package com.iupc.Mapper;
+
+import com.iupc.pojo.Users;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@Mapper
+public interface UsersMapper {
+    Users getUserByName(Users user);
+}
